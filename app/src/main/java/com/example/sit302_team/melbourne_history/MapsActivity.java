5b6eaf14caa952f4449f1f0bd286e376dc00177a
michelyousef <micheljacop@yahoo.com.au>
2016-11-13 18:16:28 -0800

    Initial commit

---
+++ app/src/main/java/com/example/sit302_team/melbourne_history/MapsActivity.java
@@ -0,0 +1,52 @@
+package com.example.sit302_team.melbourne_history;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        mMap.setIndoorEnabled(true);
+
+
+
+        // Add a marker in Melbourne and move the camera, then hiding the marker by setting the visible method to false
+        // as we need to zoom in to Melbourne without adding any icons when the application is starting
+        LatLng melbourne = new LatLng(-37.8136, 144.9631);
+        mMap.addMarker(new MarkerOptions().position(melbourne).visible(false));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(melbourne));
+        mMap.animateCamera( CameraUpdateFactory.zoomTo( 9.0f ) );
+    }
+}
