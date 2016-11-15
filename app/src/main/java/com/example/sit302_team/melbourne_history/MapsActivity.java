ab52cd497def532f28b93dca96a07ada7e258a35
michelyousef <micheljacop@yahoo.com.au>
2016-11-15 03:17:39 -0800

    Updated MainActivity and gradle

--- app/src/main/java/com/example/sit302_team/melbourne_history/MapsActivity.java
+++ app/src/main/java/com/example/sit302_team/melbourne_history/MapsActivity.java
@@ -2,7 +2,10 @@ package com.example.sit302_team.melbourne_history;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -13,11 +16,13 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        checkPlayServices();
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -49,4 +54,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.moveCamera(CameraUpdateFactory.newLatLng(melbourne));
         mMap.animateCamera( CameraUpdateFactory.zoomTo( 9.0f ) );
     }
+    // Stack Overflow available at http://stackoverflow.com/questions/31016722/googleplayservicesutil-vs-googleapiavailability
+    private boolean checkPlayServices() {
+        GoogleApiAvailability googleAPI = GoogleApiAvailability.getInstance();
+        int result = googleAPI.isGooglePlayServicesAvailable(this);
+        if(result != ConnectionResult.SUCCESS) {
+            if(googleAPI.isUserResolvableError(result)) {
+                googleAPI.getErrorDialog(this, result,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+                Toast.makeText(this, "please download the up to date google play service and try again", Toast.LENGTH_SHORT).show();
+            }
+
+            return false;
+        }
+
+        return true;
+    }
+
 }
